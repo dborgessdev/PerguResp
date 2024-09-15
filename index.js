0f1d21772c3df20e86256f69bdbcff84107fe060
Davi Borges <107523678+dborgessdev@users.noreply.github.com>
2024-09-15 10:35:18 -0300

    feat: :zap: links dinamicos para o botão de resposta vinculando cada botão a sua respectiva pergunta

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 /* * para instalar o nodemon npm install -g nodemon
-* para instalar o sequelize: npm install --save sequelize
-*  para importar mysql2: npm install --save mysql2
-* importar o express, depois cria o app com express, por fim importa o 
-* body-parser (precisa ser instalado com o comando npm install body-parser --save)
-* o body-parser é responsável por receber o corpo da requisição e transformar em um objeto de varios formatos (JSON, XML, etc). */
+ * para instalar o sequelize: npm install --save sequelize
+ *  para importar mysql2: npm install --save mysql2
+ * importar o express, depois cria o app com express, por fim importa o
+ * body-parser (precisa ser instalado com o comando npm install body-parser --save)
+ * o body-parser é responsável por receber o corpo da requisição e transformar em um objeto de varios formatos (JSON, XML, etc). */
 
-const express = require("express"); 
-const app = express(); // 
-const bodyParser = require("body-parser"); 
+const express = require("express");
+const app = express(); //
+const bodyParser = require("body-parser");
 const Pergunta = require("./database/Pergunta");
 
 //database: importa o sequelize
 const connection = require("./database/database");
+const { where } = require("sequelize");
+const { Where } = require("sequelize/lib/utils");
 //promise: usado para tratar erros
 connection
   .authenticate()
@@ -23,12 +25,12 @@ connection
   });
 
 // seta o motor de views como ejs, seta o diretorio publico para acessar os arquivos estaticos
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // habilita o body-parser, permite a leitura de json através do body-parser
-app.use(bodyParser.urlencoded({extended: false})); 
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /* A seguir, definimos uma rota que responde a requisições GET enviadas para o endpoint raiz ("/"). Isso significa que, quando o usuário acessa o endereço principal do site, esta função será chamada.
 
@@ -38,43 +40,55 @@ O parâmetro {raw: true} faz com que os resultados sejam retornados como objetos
 
 Como findAll() retorna uma Promise, o método .then() é usado para lidar com o resultado quando a consulta for bem-sucedida.*/
 app.get("/", (req, res) => {
-  Pergunta.findAll({raw: true, order: [['id', 'DESC']]}).then((perguntas) => { // seleciona todas as perguntas
+  Pergunta.findAll({ raw: true, order: [["id", "DESC"]] }).then((perguntas) => {
+    // seleciona todas as perguntas
     res.render("index", {
-      perguntas: perguntas
-   });
+      perguntas: perguntas,
+    });
   });
 });
 
-app.get("/perguntar", (req, res)=>{ // rota para perguntar
+app.get("/perguntar", (req, res) => {
+  // rota para perguntar
   res.render("perguntar");
 });
 
-app.post("/formpergunta", (req, res) => { // rota para receber a pergunta
-  var titulo = req.body.pergunta;  // Alterar para titulo
+app.post("/formpergunta", (req, res) => {
+  // rota para receber a pergunta
+  var titulo = req.body.pergunta; // Alterar para titulo
   var descricao = req.body.descricao; // Alterar para descricão
   Pergunta.create({
     titulo: titulo,
-    descricao: descricao
-  }).then(() => { // insere a pergunta no banco
-    res.redirect("/"); // redireciona para a rota principal
-  }).catch((erro) => { // caso ocorra um erro
-    console.log(erro); // mostra o erro
+    descricao: descricao,
+  })
+    .then(() => {
+      // insere a pergunta no banco
+      res.redirect("/"); // redireciona para a rota principal
+    })
+    .catch((erro) => {
+      // caso ocorra um erro
+      console.log(erro); // mostra o erro
+    });
+});
+
+//rota criada para a exibição de uma pergunta específica selecionada pelo ID passado como parâmetro pelo req.params, essa requesição é recebida através da rota /pergunta/:id que rendereiza o arquivo pergunta.ejs
+app.get("/pergunta/:id", (req, res) => { 
+  var id = req.params.id; 
+  Pergunta.findOne({ // findOne: seleciona apenas uma pergunta no banco database/Pergunta.js
+    where: { id : id}
+  }).then((pergunta) => { // o then: se a pergunta for selecionada com sucesso
+    if(pergunta != undefined){ 
+      res.render("pergunta",{
+        pergunta: pergunta
+      });
+ 
+    }else{
+      res.redirect("/")
+    }
   });
 });
-/*realizando requisição de parametros pelo http
-rota raiz//
-app.get("/:nome/:lang", (req, res) => {
-var nome = req.params.nome; // variáveis passadas para o ejs
-var lang = req.params.lang;
-var exibirMsg = true; // variável para exibir a mensagem
-  res.render("index", {
-    // renderiza o ejs
-    nome: nome, // passa a variável para o ejs
-    lang: lang, // passa a variável para o ejs
-    msg: exibirMsg, // passa a variável para o ejs
-  });
-});*/
+
 
 app.listen(8080, () => {
-  console.log("Servidor iniciado na porta 8080"); 
+  console.log("Servidor iniciado na porta 8080");
 });
